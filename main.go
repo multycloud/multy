c6a7322363d61b2f7b3ef90cc6d86cf10cfefcf8
Joao Coelho <joao.coelho@pollinate.co.uk>
2022-01-12 00:37:28 +0000

    added logging

--- main.go
+++ main.go
@@ -11,9 +11,11 @@ import (
 	"os"
 	"os/exec"
 	"path/filepath"
+	"time"
 )
 
 func main() {
+	start := time.Now()
 	log.SetFlags(log.Lshortfile)
 
 	var commandLineVars variables.CommandLineVariables
@@ -31,6 +33,7 @@ func main() {
 		}
 		for _, file := range files {
 			if !file.IsDir() && filepath.Ext(file.Name()) == ".mt" {
+				log.Println("config file found:", file.Name())
 				configFiles = append(configFiles, file.Name())
 			}
 		}
@@ -50,4 +53,6 @@ func main() {
 	d1 := []byte(hclOutput)
 	_ = os.WriteFile(*outputFile, d1, 0644)
 	_ = exec.Command("terraform", "fmt", *outputFile)
+	log.Printf("multy finished running: %s\n\n", time.Since(start).Round(time.Second))
+	log.Println("output file:", *outputFile)
 }
