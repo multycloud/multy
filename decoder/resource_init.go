ebf0fd64a8e68700a11a93384a591bbc3022209a
Gonçalo Rodrigues <goncalo.alfredo.rodrigues@gmail.com>
2022-01-15 10:52:31 +0100

    Move resource init to different file. (#26)

---
+++ decoder/resource_init.go
@@ -0,0 +1,39 @@
+package decoder
+
+import (
+	"fmt"
+	"multy-go/resources"
+	rg "multy-go/resources/resource_group"
+	"multy-go/resources/types"
+)
+
+func InitResource(resourceType string, commonParams *resources.CommonResourceParams) (resources.Resource, error) {
+	switch resourceType {
+	case "virtual_network":
+		return &types.VirtualNetwork{CommonResourceParams: commonParams}, nil
+	case "subnet":
+		return &types.Subnet{CommonResourceParams: commonParams}, nil
+	case "network_security_group":
+		return &types.NetworkSecurityGroup{CommonResourceParams: commonParams}, nil
+	case "virtual_machine":
+		return &types.VirtualMachine{CommonResourceParams: commonParams}, nil
+	case "public_ip":
+		return &types.PublicIp{CommonResourceParams: commonParams}, nil
+	case "route_table":
+		return &types.RouteTable{CommonResourceParams: commonParams}, nil
+	case "route_table_association":
+		return &types.RouteTableAssociation{CommonResourceParams: commonParams}, nil
+	case "network_interface":
+		return &types.NetworkInterface{CommonResourceParams: commonParams}, nil
+	case "database":
+		return &types.Database{CommonResourceParams: commonParams}, nil
+	case "resource_group":
+		return &rg.Type{ResourceId: commonParams.ResourceId}, nil
+	case "object_storage":
+		return &types.ObjectStorage{CommonResourceParams: commonParams}, nil
+	case "object_storage_object":
+		return &types.ObjectStorageObject{CommonResourceParams: commonParams}, nil
+	default:
+		return nil, fmt.Errorf("unknown resource type %s", resourceType)
+	}
+}
