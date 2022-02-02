ba1f36c4e554b182a57556bbd68b5fc95909e596
Gonçalo Rodrigues <goncalo.alfredo.rodrigues@gmail.com>
2022-02-02 16:49:09 +0100

    Add access to private blobs in az lambda (#42)

--- test/lambda/lambda_source_code_obj/main.hcl
+++ test/lambda/lambda_source_code_obj/main.hcl
@@ -1,20 +1,35 @@
-multy "lambda" "test" {
-  function_name = "multyfunobj"
+# TODO: refactor time dependencies so we can test this
+#multy "lambda" "function1" {
+#  function_name = "privatemultyfun"
+#  runtime = "python3.9"
+#  source_code_object = private_source_code
+#}
+
+multy "lambda" "function2" {
+  function_name = "publicmultyfun"
   runtime = "python3.9"
-  source_code_object = source_code
+  source_code_object = public_source_code
 }
 
 multy "object_storage" "obj_storage" {
-  name          = "test-storage-9999919"
+  name          = "function-storage-1722"
   random_suffix = false
 }
 
-multy "object_storage_object" "source_code" {
+#multy "object_storage_object" "private_source_code" {
+#  name                = "source_code.zip"
+#  object_storage      = obj_storage
+#  source              = cloud_specific_value({aws: "source_dir/aws_code.zip", azure: "source_dir/azure_code.zip"})
+#  acl                 = "private"
+#}
+
+multy "object_storage_object" "public_source_code" {
   name                = "source_code.zip"
   object_storage      = obj_storage
   source              = cloud_specific_value({aws: "source_dir/aws_code.zip", azure: "source_dir/azure_code.zip"})
   acl                 = "public_read"
 }
+
 
 config {
   location = var.location
--- test/lambda/lambda_source_code_obj/main.tf
+++ test/lambda/lambda_source_code_obj/main.tf
@@ -1,39 +1,64 @@
-resource "aws_s3_bucket" "obj_storage_aws" {
-  bucket = "test-storage-9999919"
+resource "aws_lambda_function" "function2_aws" {
+  tags =  {
+    Name = "publicmultyfun"
+  }
+
+  function_name = "publicmultyfun"
+  role          = aws_iam_role.iam_for_lambda_function2.arn
+  runtime       = "python3.9"
+  handler       = "lambda_function.lambda_handler"
+  s3_bucket     = aws_s3_bucket.obj_storage_aws.id
+  s3_key        = aws_s3_bucket_object.public_source_code_aws.key
 }
-resource "aws_s3_bucket_object" "source_code_aws" {
+resource "aws_iam_role" "iam_for_lambda_function2" {
+  tags =  {
+    Name = "iam_for_lambda_function2"
+  }
+
+  name               = "iam_for_lambda_function2"
+  assume_role_policy = "{\"Version\": \"2012-10-17\",\"Statement\": [{\"Action\": \"sts:AssumeRole\",\"Principal\": {\"Service\": \"lambda.amazonaws.com\"},\"Effect\": \"Allow\",\"Sid\": \"\"}]}"
+}
+resource "aws_s3_bucket" "obj_storage_aws" {
+  bucket = "function-storage-1722"
+}
+resource "aws_s3_bucket_object" "public_source_code_aws" {
   bucket = aws_s3_bucket.obj_storage_aws.id
   key    = "source_code.zip"
   acl    = "public-read"
   source = "source_dir/aws_code.zip"
 }
-resource "aws_lambda_function" "test_aws" {
-  tags =  {
-    Name = "multyfunobj"
-  }
-
-  function_name = "multyfunobj"
-  role          = aws_iam_role.iam_for_lambda_test.arn
-  runtime       = "python3.9"
-  handler       = "lambda_function.lambda_handler"
-  s3_bucket     = aws_s3_bucket.obj_storage_aws.id
-  s3_key        = aws_s3_bucket_object.source_code_aws.key
-}
-resource "aws_iam_role" "iam_for_lambda_test" {
-  tags =  {
-    Name = "iam_for_lambda_test"
-  }
-
-  name               = "iam_for_lambda_test"
-  assume_role_policy = "{\"Version\": \"2012-10-17\",\"Statement\": [{\"Action\": \"sts:AssumeRole\",\"Principal\": {\"Service\": \"lambda.amazonaws.com\"},\"Effect\": \"Allow\",\"Sid\": \"\"}]}"
-}
 resource "azurerm_resource_group" "fun-rg" {
   name     = "fun-rg"
   location = "northeurope"
 }
+resource "azurerm_function_app" "function2_azure" {
+  resource_group_name        = azurerm_resource_group.fun-rg.name
+  name                       = "publicmultyfun"
+  location                   = "northeurope"
+  storage_account_name       = azurerm_storage_account.obj_storage_azure.name
+  storage_account_access_key = azurerm_storage_account.obj_storage_azure.primary_access_key
+  app_service_plan_id        = azurerm_app_service_plan.function2_azure.id
+  os_type                    = "linux"
+
+  app_settings =  {
+    WEBSITE_RUN_FROM_PACKAGE = "${azurerm_storage_blob.public_source_code_azure.url}"
+  }
+}
+resource "azurerm_app_service_plan" "function2_azure" {
+  resource_group_name = azurerm_resource_group.fun-rg.name
+  name                = "publicmultyfunsvpl492v"
+  location            = "northeurope"
+  kind                = "Linux"
+  reserved            = true
+
+  sku {
+    tier = "Dynamic"
+    size = "Y1"
+  }
+}
 resource "azurerm_storage_account" "obj_storage_azure" {
   resource_group_name      = azurerm_resource_group.st-rg.name
-  name                     = "teststorage9999919"
+  name                     = "functionstorage1722"
   location                 = "northeurope"
   account_tier             = "Standard"
   account_replication_type = "GZRS"
@@ -49,7 +74,7 @@ resource "azurerm_storage_container" "obj_storage_azure_private" {
   storage_account_name  = azurerm_storage_account.obj_storage_azure.name
   container_access_type = "private"
 }
-resource "azurerm_storage_blob" "source_code_azure" {
+resource "azurerm_storage_blob" "public_source_code_azure" {
   name                   = "source_code.zip"
   storage_account_name   = azurerm_storage_account.obj_storage_azure.name
   storage_container_name = azurerm_storage_container.obj_storage_azure_public.name
@@ -59,31 +84,6 @@ resource "azurerm_storage_blob" "source_code_azure" {
 resource "azurerm_resource_group" "st-rg" {
   name     = "st-rg"
   location = "northeurope"
-}
-resource "azurerm_function_app" "test_azure" {
-  resource_group_name        = azurerm_resource_group.fun-rg.name
-  name                       = "multyfunobj"
-  location                   = "northeurope"
-  storage_account_name       = azurerm_storage_account.obj_storage_azure.name
-  storage_account_access_key = azurerm_storage_account.obj_storage_azure.primary_access_key
-  app_service_plan_id        = azurerm_app_service_plan.test_azure.id
-  os_type                    = "linux"
-
-  app_settings =  {
-    WEBSITE_RUN_FROM_PACKAGE = "${azurerm_storage_blob.source_code_azure.url}"
-  }
-}
-resource "azurerm_app_service_plan" "test_azure" {
-  resource_group_name = azurerm_resource_group.fun-rg.name
-  name                = "multyfunobjsvpl3ub6"
-  location            = "northeurope"
-  kind                = "Linux"
-  reserved            = true
-
-  sku {
-    tier = "Dynamic"
-    size = "Y1"
-  }
 }
 provider "aws" {
   region = "eu-west-1"
