d3db15bfcbca5003a4d280ff07caeac34f8af4b3
Gonçalo Rodrigues <goncalo.alfredo.rodrigues@gmail.com>
2022-03-24 14:27:25 +0100

    Implement outputs for vms and kubernetes (#111)

--- api/services/kubernetes_cluster/kubernetes_cluster.go
+++ api/services/kubernetes_cluster/kubernetes_cluster.go
@@ -1,12 +1,15 @@
 package kubernetes_cluster
 
 import (
+	"fmt"
 	"github.com/multycloud/multy/api/proto/common"
 	"github.com/multycloud/multy/api/proto/resources"
 	"github.com/multycloud/multy/api/services"
 	"github.com/multycloud/multy/api/util"
 	"github.com/multycloud/multy/db"
 	"github.com/multycloud/multy/resources/output"
+	"github.com/multycloud/multy/resources/output/kubernetes_service"
+	common_util "github.com/multycloud/multy/util"
 )
 
 type KubernetesClusterService struct {
@@ -16,10 +19,16 @@ type KubernetesClusterService struct {
 func (s KubernetesClusterService) Convert(resourceId string, args []*resources.CloudSpecificKubernetesClusterArgs, state *output.TfState) (*resources.KubernetesClusterResource, error) {
 	var result []*resources.CloudSpecificKubernetesClusterResource
 	for _, r := range args {
+		endpoint, err := getEndpoint(resourceId, state, r.CommonParameters.CloudProvider)
+		if err != nil {
+			return nil, err
+		}
 		result = append(result, &resources.CloudSpecificKubernetesClusterResource{
 			CommonParameters: util.ConvertCommonParams(r.CommonParameters),
 			Name:             r.Name,
 			SubnetIds:        r.SubnetIds,
+
+			Endpoint: endpoint,
 		})
 	}
 
@@ -27,6 +36,26 @@ func (s KubernetesClusterService) Convert(resourceId string, args []*resources.C
 		CommonParameters: &common.CommonResourceParameters{ResourceId: resourceId},
 		Resources:        result,
 	}, nil
+}
+
+func getEndpoint(resourceId string, state *output.TfState, cloud common.CloudProvider) (string, error) {
+	rId := common_util.GetTfResourceId(resourceId, cloud.String())
+	switch cloud {
+	case common.CloudProvider_AWS:
+		values, err := state.GetValues(kubernetes_service.AwsEksCluster{}, rId)
+		if err != nil {
+			return "", err
+		}
+		return values["endpoint"].(string), nil
+	case common.CloudProvider_AZURE:
+		values, err := state.GetValues(kubernetes_service.AzureEksCluster{}, rId)
+		if err != nil {
+			return "", err
+		}
+		return values["kube_config"].([]interface{})[0].(map[string]interface{})["host"].(string), nil
+	}
+
+	return "", fmt.Errorf("unknown cloud: %s", cloud.String())
 }
 
 func NewKubernetesClusterService(database *db.Database) KubernetesClusterService {
--- api/services/virtual_machine/virtual_machine.go
+++ api/services/virtual_machine/virtual_machine.go
@@ -1,12 +1,17 @@
 package virtual_machine
 
 import (
+	"fmt"
 	"github.com/multycloud/multy/api/proto/common"
 	"github.com/multycloud/multy/api/proto/resources"
 	"github.com/multycloud/multy/api/services"
 	"github.com/multycloud/multy/api/util"
 	"github.com/multycloud/multy/db"
 	"github.com/multycloud/multy/resources/output"
+	"github.com/multycloud/multy/resources/output/iam"
+	"github.com/multycloud/multy/resources/output/public_ip"
+	"github.com/multycloud/multy/resources/output/virtual_machine"
+	common_util "github.com/multycloud/multy/util"
 )
 
 type VirtualMachineService struct {
@@ -16,6 +21,14 @@ type VirtualMachineService struct {
 func (s VirtualMachineService) Convert(resourceId string, args []*resources.CloudSpecificVirtualMachineArgs, state *output.TfState) (*resources.VirtualMachineResource, error) {
 	var result []*resources.CloudSpecificVirtualMachineResource
 	for _, r := range args {
+		ip, err := getPublicIp(resourceId, state, r.CommonParameters.CloudProvider)
+		if err != nil {
+			return nil, err
+		}
+		identityId, err := getIdentityId(resourceId, state, r.CommonParameters.CloudProvider)
+		if err != nil {
+			return nil, err
+		}
 		result = append(result, &resources.CloudSpecificVirtualMachineResource{
 			CommonParameters:        util.ConvertCommonParams(r.CommonParameters),
 			Name:                    r.Name,
@@ -28,6 +41,9 @@ func (s VirtualMachineService) Convert(resourceId string, args []*resources.Clou
 			PublicSshKey:            r.PublicSshKey,
 			PublicIpId:              r.PublicIpId,
 			GeneratePublicIp:        r.GeneratePublicIp,
+
+			PublicIp:   ip,
+			IdentityId: identityId,
 		})
 	}
 
@@ -35,6 +51,46 @@ func (s VirtualMachineService) Convert(resourceId string, args []*resources.Clou
 		CommonParameters: &common.CommonResourceParameters{ResourceId: resourceId},
 		Resources:        result,
 	}, nil
+}
+
+func getPublicIp(resourceId string, state *output.TfState, cloud common.CloudProvider) (string, error) {
+	rId := common_util.GetTfResourceId(resourceId, cloud.String())
+	switch cloud {
+	case common.CloudProvider_AWS:
+		values, err := state.GetValues(virtual_machine.AwsEC2{}, rId)
+		if err != nil {
+			return "", err
+		}
+		return values["public_ip"].(string), nil
+	case common.CloudProvider_AZURE:
+		values, err := state.GetValues(public_ip.AzurePublicIp{}, rId)
+		if err != nil {
+			return "", err
+		}
+		return values["ip_address"].(string), nil
+	}
+
+	return "", fmt.Errorf("unknown cloud: %s", cloud.String())
+}
+
+func getIdentityId(resourceId string, state *output.TfState, cloud common.CloudProvider) (string, error) {
+	rId := common_util.GetTfResourceId(resourceId, cloud.String())
+	switch cloud {
+	case common.CloudProvider_AWS:
+		values, err := state.GetValues(iam.AwsIamRole{}, rId)
+		if err != nil {
+			return "", err
+		}
+		return values["id"].(string), nil
+	case common.CloudProvider_AZURE:
+		values, err := state.GetValues(virtual_machine.AzureVirtualMachine{}, rId)
+		if err != nil {
+			return "", err
+		}
+		return values["identity"].([]interface{})[0].(map[string]interface{})["principal_id"].(string), nil
+	}
+
+	return "", fmt.Errorf("unknown cloud: %s", cloud.String())
 }
 
 func NewVirtualMachineService(database *db.Database) VirtualMachineService {
