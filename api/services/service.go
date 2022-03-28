b0ae971f7a93f02f246f557ba8569dc29a2c740e
goncalo-rodrigues <goncalo.alfredo.rodrigues@gmail.com>
2022-03-28 14:49:17 +0200

    Fix null ctx in update and delete

--- api/services/service.go
+++ api/services/service.go
@@ -132,7 +132,7 @@ func (s Service[Arg, OutT]) update(ctx context.Context, in UpdateRequest[Arg]) (
 		return *new(OutT), err
 	}
 
-	_, err = deploy.Deploy(nil, c, previousResource, find(c, in.GetResourceId()))
+	_, err = deploy.Deploy(ctx, c, previousResource, find(c, in.GetResourceId()))
 	if err != nil {
 		return *new(OutT), err
 	}
@@ -164,7 +164,7 @@ func (s Service[Arg, OutT]) delete(ctx context.Context, in WithResourceId) (*com
 		return nil, err
 	}
 
-	_, err = deploy.Deploy(nil, c, previousResource, nil)
+	_, err = deploy.Deploy(ctx, c, previousResource, nil)
 	if err != nil {
 		return nil, err
 	}
