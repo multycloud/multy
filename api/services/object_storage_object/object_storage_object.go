849cc23accb972133fe9cb9f26018f55e4fe73e7
Joao Coelho <joao.msc.coelho@gmail.com>
2022-04-15 18:43:20 +0100

    cleanup

--- api/services/object_storage_object/object_storage_object.go
+++ api/services/object_storage_object/object_storage_object.go
@@ -1,14 +1,11 @@
 package object_storage_object
 
 import (
-	"fmt"
-	"github.com/multycloud/multy/api/proto/commonpb"
 	"github.com/multycloud/multy/api/proto/resourcespb"
 	"github.com/multycloud/multy/api/services"
 	"github.com/multycloud/multy/api/util"
 	"github.com/multycloud/multy/db"
 	"github.com/multycloud/multy/resources/output"
-	"github.com/multycloud/multy/resources/output/object_storage_object"
 )
 
 type ObjectStorageObjectService struct {
@@ -36,23 +33,4 @@ func NewObjectStorageObjectService(database *db.Database) ObjectStorageObjectSer
 	}
 	nsg.Service.Converters = &nsg
 	return nsg
-}
-
-func getUrl(resourceId string, state *output.TfState, cloud commonpb.CloudProvider) (string, error) {
-	switch cloud {
-	case commonpb.CloudProvider_AWS:
-		values, err := state.GetValues(object_storage_object.AwsS3BucketObject{}, resourceId)
-		if err != nil {
-			return "", err
-		}
-		return fmt.Sprintf("https://%s.s3.amazonaws.com/%s", values["bucket"].(string), values["name"].(string)), nil
-	case commonpb.CloudProvider_AZURE:
-		values, err := state.GetValues(object_storage_object.AzureStorageAccountBlob{}, resourceId)
-		if err != nil {
-			return "", err
-		}
-		return values["url"].(string), nil
-	}
-
-	return "", fmt.Errorf("unknown cloud: %s", cloud.String())
 }
