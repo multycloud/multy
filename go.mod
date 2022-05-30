c3eb72cd48d7e779132fe168efb971338d89c0ba
Matt Vella <mattvella07@gmail.com>
2022-05-30 16:47:19 -0500

    Adding more VM sizes

--- go.mod
+++ go.mod
@@ -13,6 +13,7 @@ require (
 	github.com/spf13/pflag v1.0.5
 	github.com/stretchr/testify v1.7.1
 	github.com/zclconf/go-cty v1.10.0
+	golang.org/x/crypto v0.0.0-20210921155107-089bfa567519
 	golang.org/x/exp v0.0.0-20220218215828-6cf2b201936e
 	google.golang.org/genproto v0.0.0-20210602131652-f16073e35f0c
 	google.golang.org/grpc v1.45.0
@@ -34,7 +35,6 @@ require (
 	github.com/mitchellh/go-wordwrap v1.0.1 // indirect
 	github.com/pmezard/go-difflib v1.0.0 // indirect
 	github.com/stretchr/objx v0.1.1 // indirect
-	golang.org/x/crypto v0.0.0-20210921155107-089bfa567519 // indirect
 	golang.org/x/net v0.0.0-20220127200216-cd36cc0744dd // indirect
 	golang.org/x/sys v0.0.0-20211216021012-1d35b9e2eb4e // indirect
 	golang.org/x/xerrors v0.0.0-20200804184101-5ec99f83aff1 // indirect
--- resources/common/vm_size.go
+++ resources/common/vm_size.go
@@ -17,6 +17,50 @@ var VMSIZE = map[commonpb.VmSize_Enum]map[commonpb.CloudProvider]string{
 		commonpb.CloudProvider_AWS:   "t2.large",
 		commonpb.CloudProvider_AZURE: "Standard_D2as_v4",
 	},
+	commonpb.VmSize_GENERAL_NANO: {
+		commonpb.CloudProvider_AWS:   "t2.nano",
+		commonpb.CloudProvider_AZURE: "Standard_B1ls",
+	},
+	commonpb.VmSize_GENERAL_LARGE: {
+		commonpb.CloudProvider_AWS:   "t2.large",
+		commonpb.CloudProvider_AZURE: "Standard_B2ms",
+	},
+	commonpb.VmSize_GENERAL_XLARGE: {
+		commonpb.CloudProvider_AWS:   "t2.xlarge",
+		commonpb.CloudProvider_AZURE: "Standard_B4ms",
+	},
+	commonpb.VmSize_GENERAL_2XLARGE: {
+		commonpb.CloudProvider_AWS:   "t2.2xlarge",
+		commonpb.CloudProvider_AZURE: "Standard_B8ms",
+	},
+	commonpb.VmSize_COMPUTE_LARGE: {
+		commonpb.CloudProvider_AWS:   "c7g.large",
+		commonpb.CloudProvider_AZURE: "Standard_F2s_v2",
+	},
+	commonpb.VmSize_COMPUTE_XLARGE: {
+		commonpb.CloudProvider_AWS:   "c7g.xlarge",
+		commonpb.CloudProvider_AZURE: "Standard_F4s_v2",
+	},
+	commonpb.VmSize_COMPUTE_2XLARGE: {
+		commonpb.CloudProvider_AWS:   "c7g.2xlarge",
+		commonpb.CloudProvider_AZURE: "Standard_F8s_v2",
+	},
+	commonpb.VmSize_COMPUTE_4XLARGE: {
+		commonpb.CloudProvider_AWS:   "c7g.4xlarge",
+		commonpb.CloudProvider_AZURE: "Standard_F16s_v2",
+	},
+	commonpb.VmSize_COMPUTE_8XLARGE: {
+		commonpb.CloudProvider_AWS:   "c7g.8xlarge",
+		commonpb.CloudProvider_AZURE: "Standard_F32s_v2",
+	},
+	commonpb.VmSize_COMPUTE_12XLARGE: {
+		commonpb.CloudProvider_AWS:   "c7g.12xlarge",
+		commonpb.CloudProvider_AZURE: "Standard_F48s_v2",
+	},
+	commonpb.VmSize_COMPUTE_16XLARGE: {
+		commonpb.CloudProvider_AWS:   "c7g.16xlarge",
+		commonpb.CloudProvider_AZURE: "Standard_F64s_v2",
+	},
 }
 
 const (
