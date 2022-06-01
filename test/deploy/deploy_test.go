af130d291dc554ea48fda732db2bd903bc697429
goncalo-rodrigues <goncalo.alfredo.rodrigues@gmail.com>
2022-06-01 15:44:46 +0100

    chore: add tests for deploy.go

--- test/deploy/deploy_test.go
+++ test/deploy/deploy_test.go
@@ -23,10 +23,7 @@ import (
 
 // TODO: tests
 // - provider from prev resource is included
-// - affected resources is used in terraform commands
-// - terraform apply is called
 // - get state calls terraform show
-// - refresh calls terraform refresh
 // - init calls terraform init
 
 func init() {
@@ -109,4 +106,150 @@ func TestDeploy_rollbacksIfSomethingFails(t *testing.T) {
 		t.Fatalf("can't read main.tf, %s", err)
 	}
 	assert.NotContains(t, string(file), res.GetResourceId())
+}
+
+func TestDeploy_callsTfApply(t *testing.T) {
+	ctx := mockCreds(t)
+	mockTfCmd := &MockTerraformCommand{}
+	sut := deploy.DeploymentExecutor{
+		TfCmd: mockTfCmd,
+	}
+
+	config, err := resources.LoadConfig(&configpb.Config{
+		UserId:          "test",
+		Resources:       nil,
+		ResourceCounter: 1,
+	}, types.Metadatas)
+	if err != nil {
+		t.Fatalf("can't load config, %s", err)
+	}
+
+	res, err := config.CreateResource(&resourcespb.VirtualNetworkArgs{
+		CommonParameters: &commonpb.ResourceCommonArgs{
+			Location:      commonpb.Location_US_EAST_1,
+			CloudProvider: commonpb.CloudProvider_AZURE,
+		},
+		Name:      "test-vn",
+		CidrBlock: "10.0.0.0/16",
+	})
+	if err != nil {
+		t.Fatalf("can't create resource, %s", err)
+	}
+
+	mockTfCmd.
+		On("Init", mock.Anything, mock.Anything).
+		Return(nil)
+	mockTfCmd.
+		On("Apply", mock.Anything, mock.Anything, mock.Anything).
+		Return(nil)
+	mockTfCmd.
+		On("GetState", mock.Anything, mock.Anything).
+		Return(&output.TfState{}, nil)
+	_, err = sut.Deploy(ctx, config, nil, nil)
+	if err != nil {
+		t.Fatalf("can't deploy, %s", err)
+	}
+
+	mockTfCmd.AssertNumberOfCalls(t, "Apply", 1)
+	// assert that in the end the main.tf contains the resource id
+	file, err := os.ReadFile(filepath.Join(deploy.GetTempDirForUser(false, "test"), "main.tf"))
+	assert.NoError(t, err, "unable to read main.tf")
+	assert.Contains(t, string(file), res.GetResourceId())
+}
+
+func TestDeploy_onlyAffectedResources(t *testing.T) {
+	ctx := mockCreds(t)
+	mockTfCmd := &MockTerraformCommand{}
+	sut := deploy.DeploymentExecutor{
+		TfCmd: mockTfCmd,
+	}
+
+	config, err := resources.LoadConfig(&configpb.Config{
+		UserId:          "test",
+		Resources:       nil,
+		ResourceCounter: 1,
+	}, types.Metadatas)
+	if err != nil {
+		t.Fatalf("can't load config, %s", err)
+	}
+
+	r1, err := config.CreateResource(&resourcespb.VirtualNetworkArgs{
+		CommonParameters: &commonpb.ResourceCommonArgs{
+			Location:      commonpb.Location_US_EAST_1,
+			CloudProvider: commonpb.CloudProvider_AZURE,
+		},
+		Name:      "test-vn",
+		CidrBlock: "10.0.0.0/16",
+	})
+	if err != nil {
+		t.Fatalf("can't create resource, %s", err)
+	}
+
+	mockTfCmd.
+		On("Init", mock.Anything, mock.Anything).
+		Return(nil)
+	mockTfCmd.
+		On("Apply", mock.Anything, mock.Anything, mock.Anything).
+		Return(nil).Once()
+	mockTfCmd.
+		On("GetState", mock.Anything, mock.Anything).
+		Return(&output.TfState{}, nil)
+	_, err = sut.Deploy(ctx, config, nil, r1)
+	if err != nil {
+		t.Fatalf("can't deploy, %s", err)
+	}
+
+	res2, err := config.CreateResource(&resourcespb.VirtualNetworkArgs{
+		CommonParameters: &commonpb.ResourceCommonArgs{
+			Location:      commonpb.Location_US_EAST_1,
+			CloudProvider: commonpb.CloudProvider_AWS,
+		},
+		Name:      "test-vn",
+		CidrBlock: "10.0.0.0/16",
+	})
+	if err != nil {
+		t.Fatalf("can't create resource, %s", err)
+	}
+
+	mockTfCmd.
+		On("Apply", mock.Anything, mock.Anything, mock.Anything).
+		Return(nil).Once()
+	_, err = sut.Deploy(ctx, config, nil, res2)
+	if err != nil {
+		t.Fatalf("can't deploy, %s", err)
+	}
+
+	affectedResources := config.GetAffectedResources(res2.GetResourceId())
+	assert.NotEmpty(t, affectedResources)
+	mockTfCmd.AssertCalled(t, "Apply", mock.Anything, mock.Anything, affectedResources)
+}
+
+func TestRefresh(t *testing.T) {
+	ctx := mockCreds(t)
+	mockTfCmd := &MockTerraformCommand{}
+	sut := deploy.DeploymentExecutor{
+		TfCmd: mockTfCmd,
+	}
+
+	config, err := resources.LoadConfig(&configpb.Config{
+		UserId:          "test",
+		Resources:       nil,
+		ResourceCounter: 1,
+	}, types.Metadatas)
+	if err != nil {
+		t.Fatalf("can't load config, %s", err)
+	}
+
+	mockTfCmd.
+		On("Init", mock.Anything, mock.Anything).
+		Return(nil)
+	mockTfCmd.
+		On("Refresh", mock.Anything, mock.Anything).
+		Return(nil)
+	err = sut.RefreshState(ctx, "test", config)
+	if err != nil {
+		t.Fatalf("can't refresh, %s", err)
+	}
+
+	mockTfCmd.AssertNumberOfCalls(t, "Refresh", 1)
 }
