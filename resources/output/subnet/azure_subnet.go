992e566578f841fa375a068b8014e72fdf4b540b
goncalo-rodrigues <goncalo.alfredo.rodrigues@gmail.com>
2022-06-09 09:24:29 +0100

    fix: add json tags to subnet

--- resources/output/subnet/azure_subnet.go
+++ resources/output/subnet/azure_subnet.go
@@ -9,9 +9,9 @@ const AzureResourceName = "azurerm_subnet"
 
 type AzureSubnet struct {
 	*common.AzResource `hcl:",squash" default:"name=azurerm_subnet"`
-	AddressPrefixes    []string `hcl:"address_prefixes"`
-	VirtualNetworkName string   `hcl:"virtual_network_name,expr"`
-	ServiceEndpoints   []string `hcl:"service_endpoints" hcle:"omitempty"`
+	AddressPrefixes    []string `hcl:"address_prefixes" json:"address_prefixes"`
+	VirtualNetworkName string   `hcl:"virtual_network_name,expr" json:"virtual_network_name"`
+	ServiceEndpoints   []string `hcl:"service_endpoints" hcle:"omitempty" json:"service_endpoints"`
 }
 
 func (subnet *AzureSubnet) GetId() string {
