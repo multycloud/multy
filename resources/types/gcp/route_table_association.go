d69717d7f85b029b04998e6627e9f73b0c7b62ef
Gonçalo Rodrigues <goncalo.alfredo.rodrigues@gmail.com>
2022-06-15 16:49:35 +0100

    feat: implement route tables in gcp (#318)

---
+++ resources/types/gcp/route_table_association.go
@@ -0,0 +1,37 @@
+package gcp_resources
+
+import (
+	"fmt"
+	"github.com/multycloud/multy/api/proto/commonpb"
+	"github.com/multycloud/multy/api/proto/resourcespb"
+	"github.com/multycloud/multy/resources"
+	"github.com/multycloud/multy/resources/output"
+	"github.com/multycloud/multy/resources/types"
+)
+
+type GcpRouteTableAssociation struct {
+	*types.RouteTableAssociation
+}
+
+func InitRouteTableAssociation(vn *types.RouteTableAssociation) resources.ResourceTranslator[*resourcespb.RouteTableAssociationResource] {
+	return GcpRouteTableAssociation{vn}
+}
+
+func (r GcpRouteTableAssociation) FromState(_ *output.TfState) (*resourcespb.RouteTableAssociationResource, error) {
+	return &resourcespb.RouteTableAssociationResource{
+		CommonParameters: &commonpb.CommonChildResourceParameters{
+			ResourceId:  r.ResourceId,
+			NeedsUpdate: false,
+		},
+		SubnetId:     r.Args.SubnetId,
+		RouteTableId: r.Args.RouteTableId,
+	}, nil
+}
+
+func (r GcpRouteTableAssociation) Translate(resources.MultyContext) ([]output.TfBlock, error) {
+	return nil, nil
+}
+
+func (r GcpRouteTableAssociation) GetMainResourceName() (string, error) {
+	return "", fmt.Errorf("route table associations don't exist in gcp")
+}
