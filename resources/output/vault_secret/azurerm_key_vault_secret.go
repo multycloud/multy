8d464c0ab2413a6bce20cc3f8d65301257ed1259
goncalo-rodrigues <goncalo.alfredo.rodrigues@gmail.com>
2022-08-12 13:06:39 +0200

    feat: drift detection for vault secret

--- resources/output/vault_secret/azurerm_key_vault_secret.go
+++ resources/output/vault_secret/azurerm_key_vault_secret.go
@@ -7,5 +7,5 @@ const AzureResourceName = "azurerm_key_vault_secret"
 type AzureKeyVaultSecret struct {
 	*common.AzResource `hcl:",squash" default:"name=azurerm_key_vault_secret"`
 	KeyVaultId         string `hcl:"key_vault_id,expr"`
-	Value              string `hcl:"value"`
+	Value              string `hcl:"value" json:"value"`
 }
